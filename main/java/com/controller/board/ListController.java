5e6b8034655c2e89b02eb4025decbe4d5b6b0056
YONGGYO LEE <yonggyo1981@gmail.com>
2021-09-30 10:23:43 +0900

    .

--- main/java/com/controller/board/ListController.java
+++ main/java/com/controller/board/ListController.java
@@ -4,14 +4,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class ListController extends HttpServlet {
+	
+	private static final long serialVersionUID = -1079492929715019421L;
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		response.setContentType("text/html; charset=utf-8");
-		out.print("Å×½ºÆ®");
+		RequestDispatcher rd = request.getRequestDispatcher("../ch00/list.jsp");
+		rd.forward(request, response);
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
