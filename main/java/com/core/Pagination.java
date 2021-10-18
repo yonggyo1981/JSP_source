b6eb06c56e0c3359bb585e82557a0bb95577d4ac
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-18 15:12:01 +0900

    .

--- main/java/com/core/Pagination.java
+++ main/java/com/core/Pagination.java
@@ -15,6 +15,7 @@ public class Pagination {
 	private int num; // 구간 번호
 	private int prevNo; // 이전 구간 시작 번호
 	private int nextNo; // 다음 구간 시작 번호
+	private int lastPageNo; // 마지막 페이지 번호
 	
 	public Pagination(int page, int total) {
 		this.page = page;
@@ -30,29 +31,46 @@ public class Pagination {
 		lastNo = startNo + pageLinks - 1; // 구간별 종료 번호
 		
 		// 마지막 페이지 번호
-		int lastPageNo = (int)ceil(total / limit);
+		lastPageNo = (int)ceil(total / limit);
+		
+		// 마지막 페이지 구간
+		int lastNum = (int)floor((lastPageNo - 1) / pageLinks); 
+		
 		// 마지막 구간의 페이지 번호가 마지막 페이지 번호보다 크면 X
 		if (lastNo > lastPageNo) {  
 			lastNo = lastPageNo;
 		}
 		
 		// 이전 구간 페이지 시작 번호
-		prevNo = (num - 1) * pageLinks + 1;
+		if (num > 0) {
+			prevNo = (num - 1) * pageLinks + 1;
+		}
 		
 		// 다음 구간 페이지 시작 번호
-		nextNo = (num + 1) * pageLinks + 1;
+		if (num < lastNum) { // 현재 페이지 구간이 마지막 페이지 구간보다 이전인 경우 
+			nextNo = (num + 1) * pageLinks + 1;
+		}
 	}
 	
 	public String getPageHtml() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("<ul class='pagination'>");
 		
-		// 이전 페이지 링크 
+		// 첫 페이지 
 		sb.append("<li class='page'>");
 		sb.append("<a href='#'>");
-		sb.append(prevNo);
+		sb.append(1);
 		sb.append("</a>");
 		sb.append("</li>");
+		
+		// 이전 페이지 링크 
+		if (prevNo > 0) {
+			sb.append("<li class='page'>");
+			sb.append("<a href='#'>");
+			sb.append(prevNo);
+			sb.append("</a>");
+			sb.append("</li>");
+		}
 		
 		for (int i = startNo; i <= lastNo; i++) {
 			sb.append("<li class='page'>");
@@ -61,13 +79,21 @@ public class Pagination {
 		}
 		
 		// 다음 페이지
+		if (nextNo > 0) {
+			sb.append("<li class='page'>");
+			sb.append("<a href='#'>");
+			sb.append(nextNo);
+			sb.append("</a>");
+			sb.append("</li>");
+			sb.append("</ul>");
+		}
+		
+		// 마지막 페이지
 		sb.append("<li class='page'>");
 		sb.append("<a href='#'>");
-		sb.append(nextNo);
+		sb.append(lastPageNo);
 		sb.append("</a>");
 		sb.append("</li>");
-		
-		sb.append("</ul>");
 		
 		return sb.toString();
 	}
