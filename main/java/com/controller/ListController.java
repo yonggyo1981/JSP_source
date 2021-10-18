0fd4fb3cdb14486839d21cd82c3da2da4ea1e883
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-18 16:53:12 +0900

    .

--- main/java/com/controller/ListController.java
+++ main/java/com/controller/ListController.java
@@ -3,10 +3,23 @@ package com.controller;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.util.*;
+
+import com.dao.*;
+import com.dto.*;
+import com.core.*;
 
 public class ListController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=utf-8");
+		
+		BoardDao dao = new BoardDao();
+		ArrayList<Board> list = dao.getList(request);
+		int total = dao.getTotal();
+		
+		Pagination pagination = new Pagination(request, total);
+		String pagingHtml = pagination.getPageHtml();
+		
 		RequestDispatcher rd = request.getRequestDispatcher("/list.jsp");
 		rd.forward(request, response);
 	}
