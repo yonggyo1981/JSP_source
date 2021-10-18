400065958a7c03a08c629a96ea705ba49b52b5f3
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-18 16:27:19 +0900

    .

--- main/java/com/dto/Board.java
+++ main/java/com/dto/Board.java
@@ -1,5 +1,71 @@
 package com.dto;
 
-public class Board {
+import java.sql.*;
 
+public class Board {
+	private int idx; 
+	private String poster;
+	private String subject;
+	private String content;
+	private String regDt;
+	
+	public Board() {}
+	
+	public Board(ResultSet rs) throws SQLException {
+		this(
+			rs.getInt("idx"),
+			rs.getString("poster"),
+			rs.getString("subject"),
+			rs.getString("content"),
+			rs.getString("regDt")
+		);
+	}
+	
+	public Board(int idx, String poster, String subject, String content, String regDt) {
+		this.idx = idx;
+		this.poster = poster;
+		this.subject = subject;
+		this.content = content;
+		this.regDt = regDt;
+	}
+	
+	public int getIdx() {
+		return idx;
+	}
+	
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	
+	public String getPoster() {
+		return poster;
+	}
+	
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+	
+	public String getSubject() {
+		return subject;
+	}
+	
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public String getRegDt() {
+		return regDt;
+	}
+	
+	public void setRegDt(String regDt) {
+		this.regDt = regDt;
+	}	
 }
