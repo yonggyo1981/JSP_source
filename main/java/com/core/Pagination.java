bc08cdb4946ff401a3cde69c8dd95990435b5af9
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-18 14:42:16 +0900

    .

--- main/java/com/core/Pagination.java
+++ main/java/com/core/Pagination.java
@@ -1,16 +1,49 @@
 package com.core;
 
+import static java.lang.Math.*;
+
 /**
  * 페이지네이션 
  *
  */
 public class Pagination {
-
+	private int page; // 페이지 번호
+	private int pageLinks; // 페이지 구간별 개수
+	private int limit; //1페이지당 출력 개수
+	private int startNo; // 구간별 시작 번호
+	private int lastNo; // 구간별 종료 번호
+	private int num; // 구간 번호
+	
 	public Pagination(int page, int total) {
-		int pageLinks = 5; // 페이지 구간별 개수
-		int limit = 15; // 1페이지당 출력 개수 
+		this.page = page;
+		if (this.page <= 0) 
+			this.page = 1;
 		
-		int num = (int)Math.floor((page - 1)/pageLinks); // 페이지 구간 번호
+		pageLinks = 5; // 페이지 구간별 개수
+		limit = 15; // 1페이지당 출력 개수 
 		
+		num = (int)floor((this.page - 1)/pageLinks); // 페이지 구간 번호
+
+		startNo = num * pageLinks + 1; // 구간별 시작 번호
+		lastNo = startNo + pageLinks - 1; // 구간별 종료 번호
+	}
+	
+	public String getPageHtml() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("<ul class='pagination'>");
+		
+		for (int i = startNo; i <= lastNo; i++) {
+			sb.append("<li class='page'>");
+			sb.append(i);
+			sb.append("</li>");
+		}
+		
+		sb.append("</ul>");
+		
+		return sb.toString();
 	}
 }
+
+
+
+
--- main/webapp/paging.jsp
+++ main/webapp/paging.jsp
@@ -1,2 +1,5 @@
 <%@ page contentType="text/html; charset=utf-8" %>
 <%@ page import="com.core.Pagination" %>
+<%
+	Pagination pagination = new Pagination(7, 1000);
+%>
