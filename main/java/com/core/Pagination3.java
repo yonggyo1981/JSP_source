afb9fd4479a44d5bdf4c2c69a8742f5e22f1fdcc
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-19 16:39:21 +0900

    .

--- main/java/com/core/Pagination3.java
+++ main/java/com/core/Pagination3.java
@@ -1,6 +1,16 @@
 package com.core;
 
 public class Pagination3 {
+	
+	private int num; // 페이지 구간 번호(현재 페이지)
+	private int lastNum; // 마지막 페이지 구간번호
+	private int lastPageNo; // 마지막 페이지 번호
+	private int startNo; // 구간별 시작 번호(0번째 구간  - 1페이지)
+	private int lastNo; // 구간별 종료 번호(0번째 구간 - 5페이지)
+	private int prevNo; // 이전 구간 시작 번호
+	private int nextNo; // 다음 구간 시작 번호
+	private int page; // 현재 페이지 번호
+	
 	/**
 	 * 생성자 - 페이지 네이션 구성을 위한 각각의 요소를 구하는 
 	 *   1. 페이지 구간번호 
@@ -31,8 +41,51 @@ public class Pagination3 {
 	 * @param page - 현재 페이지 번호
 	 * @param total - 전체 레코드 개수
 	 * @param pageLinks - 구간별 페이지 표기 갯수(1 2 3 4 5 | 6 7 8 9 10 ..)
+	 * @param limit - 1페이지 당 레코드 개수
 	 */
-	public Pagination3(int page, int total, int pageLinks) {
+	public Pagination3(int page, int total, int pageLinks, int limit) {
+		page = (page <= 0)?1:page;
+		pageLinks = (pageLinks <= 0)?10:pageLinks;
+		limit = (limit <= 0)?15:limit;
 		
+		this.page = page;
+		
+		/**
+			1. 페이지 구간번호 
+		    	버림((현재 페이지 번호 - 1 ) / 구간별 페이지 표기 갯수)
+		    	Math.floor 버림 / Math.ceil 올림 / Math.round 반올림
+		*/
+		num = (int)Math.floor((page - 1) / pageLinks);
+		
+		/**
+		 * lastNum; // 마지막 페이지 구간번호 
+		 * lastPageNo; // 마지막 페이지 번호
+		 */
+		lastPageNo =  (int)Math.ceil(total / (double)limit); 
+		lastNum = (int)Math.floor((lastPageNo - 1) / pageLinks);
+		
+		/**
+		*   2. 페이지 구간에서 시작 페이지 번호, 종료 페이지 번호
+		*   	시작 페이지 번호 = (페이지 구간번호 * 구간별 페이지 표기 갯수 + 1)
+		*   	종료 페이지 번호 = (시작페이지 번호 + 구간별 페이지 표기 갯수 -1)
+		*/
+		startNo = num * pageLinks + 1; 
+		lastNo =  startNo + pageLinks - 1;
+		
+		// 마지막 페이지 구간 끝나는 페이지는 = 마지막 페이지 번호
+		if (lastNo > lastPageNo) { 
+			lastNo = lastPageNo;
+		}
+		
+		/**
+		 * 다음 시작 페이지 번호, 이전 시작 페이지 번호
+		 */
+		if (num > 0) {
+			prevNo = (num - 1) * pageLinks + 1;
+		}
+		
+		if (num < lastNum) {
+			nextNo = (num + 1) * pageLinks + 1;
+		}
 	}
 }
